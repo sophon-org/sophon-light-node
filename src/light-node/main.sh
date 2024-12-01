b84ffb2844eeedc4a3764b52fbf6075561defb14
Federico Martín Alconada Verzini <fedealconada@gmail.com>
2024-12-01 10:36:10 -0300

    chore: test

--- src/light-node/main.sh
+++ src/light-node/main.sh
@@ -419,15 +419,13 @@ run_node() {
             }
     else
         echo "test2"
-        log "$(cat <<EOF
-+-------------------------------------------------------------------------------------------------+
-| 🔔 [NOT ELIGIBLE FOR REWARDS]                                                                     |
-| 🔔 You have not provided an operator address. Your Sophon Light Node will run but not participate |
-|    in the rewards program.                                                                        |
-+-------------------------------------------------------------------------------------------------+
-EOF
-)"
-        echo "test2"
+        log "
+            +$(printf '%*s' "100" | tr ' ' '-')+
+            | 🔔 [NOT ELIGIBLE FOR REWARDS]
+            | 🔔 You have not provided an operator address. Your Sophon Light Node will run but not participate in the rewards program.
+            +$(printf '%*s' "100" | tr ' ' '-')+
+        "
+        echo "test3"
     fi
 }
 
